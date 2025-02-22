05c1f0725fe29bbaedda19f7d11248086aba7e18
amons <zhangmeng_tom@163.com>
2025-02-22 20:41:37 +0800

    fix Hero hitalker_home name -> Hitalker_home

--- components/ui/Hero/Hero.jsx
+++ components/ui/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import GradientWrapper from "@/components/GradientWrapper";
 import Image from "next/image";
 import NavLink from "../NavLink";
-import HeroHomeImg from "@/public/images/hitalker_home.png";
+import HeroHomeImg from "@/public/images/Hitalker_home.png";
 import LayoutEffect from "@/components/LayoutEffect";
 import { useLanguage } from '../../../context/LanguageContext';
 
